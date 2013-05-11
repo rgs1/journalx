1d15ebbd6ef9e5a7046c3b83c6236bd0ee151e7b
Raul Gutierrez Segales <rgs@itevenworks.net>
2013-05-11 11:28:01 -0700

    Fixed broken test due to encoding and mismatched in shared_date

--- entries_svc/tests.py
+++ entries_svc/tests.py
@@ -1,5 +1,4 @@
 from django.test import TestCase
-from django.utils import timezone
 
 from models import JournalEntry
 import json
@@ -9,17 +8,17 @@ class SimpleTest(TestCase):
         """
         Client libraries are expecting to consume this, so lets test
         """
-        d = timezone.now()
         expected_serialized_entry = {
             'title': 'test1',
             'desc': 'desc1',
-            'id': 1,
-            'shared_date': str(d)
             }
-        e = JournalEntry(title='test1',
-                         desc='desc1',
-                         shared_date=d)
-        e.save()
+        expected_reloaded = json.loads(json.dumps(expected_serialized_entry))
 
-        self.assertEqual(json.loads(json.dumps(expected_serialized_entry)),
-                         e.to_dict())
+        e = JournalEntry(title='test1',
+                         desc='desc1')
+        e.save()
+        e_as_dict = e.to_dict()
+
+        self.assertEqual(expected_reloaded['title'], e_as_dict['title'])
+        self.assertEqual(expected_reloaded['desc'], e_as_dict['desc'])
+
